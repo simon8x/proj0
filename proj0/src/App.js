bca23b6bd3c133563d92d11a7554f3cc6edbf62f
simon8x <simonibalo@gmail.com>
2021-02-04 05:52:39 -0300

    un componente sin estado en su lugar es6

--- proj0/src/App.js
+++ proj0/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import ComponenteBloque1 from './components/componentebloque1';
+import './assets/css/estilos.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/*
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,8 +19,9 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a>*/}
       </header>
+      <ComponenteBloque1/>
     </div>
   );
 }
---
+++ proj0/src/components/componentebloque1/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ComponenteBloque1 extends Component{
+    render(){
+        return(
+            <div className="borde-rojo padd10 col2p">
+                <h3>Soy el bloque 1</h3>
+                <p>un h3 y un p</p>
+            </div>
+
+        )
+    }
+}
+
+export default ComponenteBloque1
